3562308f674855eef614976fad88b72c6f341e73
wuaann <114000299+wuaann@users.noreply.github.com>
2023-06-09 15:22:35 +0700

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,81 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import '../assets/css/home.css';
+import FlowerApi from "../api/flowerApi";
 
 function Home() {
-    const [flowers, setFlowers] = useState([]);
-    const [categories, setCategories] = useState([]);
+    const [hotFlower, setHotFlower] = useState([]);
+    const [normalFlower, setNormalFlower] = useState([]);
+
 
     useEffect(() => {
-        fetch('https://647066d33de51400f7243134.mockapi.io/flower')
-            .then((response) => response.json())
-            .then((data) => {
-                setFlowers(data);
-                setCategories(getUniqueCategories(data));
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+        setHotFlower([]);
+        setNormalFlower([]);
+        const fetchFlowers = async () => {
+            try {
+                const reponse = await FlowerApi.getAll();
+                reponse.forEach((flower) => {
+                    if (flower.category) {
+                        setHotFlower(prevState => [...prevState, flower]);
+                    } else setNormalFlower(prevState => [...prevState, flower]);
 
-    const getUniqueCategories = (flowersData) => {
-        const uniqueCategories = flowersData.reduce((result, flower) => {
-            if (!result.includes(flower.category)) {
-                result.push(flower.category);
+                })
+            } catch (error) {
+                console.log('Failed to fetch flower list: ', error);
             }
-            return result;
-        }, []);
-        return uniqueCategories;
-    };
+        }
+        fetchFlowers();
 
-    const highlightedCategories = [''];
-    const oldCategories = categories.filter(
-        (category) => !highlightedCategories.includes(category)
-    );
+    }, []);
 
     return (
         <div>
-            {highlightedCategories.map((category) => (
-                <div key={category} className="category">
-                    <div className="product-grid">
-                        {flowers
-                            .filter((flower) => flower.category === category)
-                            .map((flower) => (
-                                <div key={flower.id} className="product-item">
-                                    <img
-                                        src={flower.avatar}
-                                        alt={flower.name}
-                                        className="product-image"
-                                    />
-                                    <p className="product-name">{flower.name}</p>
-                                    <p className="product-price">{flower.price}</p>
-                                </div>
-                            ))}
-                    </div>
-                </div>
-            ))}
 
-            {oldCategories.map((category) => (
-                <div key={category} className="category">
-                    <div className="pr_grid">
-                        {flowers
-                            .filter((flower) => flower.category === category)
-                            .map((flower) => (
-                                <div key={flower.id} className="pr_item">
-                                    <img
-                                        src={flower.avatar}
-                                        alt={flower.name}
-                                        className="Pr_image"
-                                    />
-                                    <p className="product-name">{flower.name}</p>
-                                    <p className="product-price">{flower.price}</p>
-                                    <button className="button_mua">Mua</button>
-                                    <button className="button_detail">Chi tiết</button>
-                                </div>
-                            ))}
+            <h1>HOT FLOWER</h1>
+            <div className="pr_grid">
+                {hotFlower.map((flower) => (
+                    <div key={flower.id} className="pr_item">
+                        <img
+                            src={flower.avatar}
+                            alt={flower.name}
+                            className="Pr_image"
+                        />
+                        <p className="product-name">{flower.name}</p>
+                        <p className="product-price">{flower.price}</p>
+                        <button className="button_mua">Mua</button>
+                        <button className="button_detail">Chi tiết</button>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
+            <h1>FLOWER</h1>
+            <div className="pr_grid">
+                {normalFlower.map((item) => (
+                    <div key={item.id} className="pr_item">
+                        <img
+                            src={item.avatar}
+                            alt={item.name}
+                            className="Pr_image"
+                        />
+                        <p className="product-name">{item.name}</p>
+                        <p className="product-price">{item.price}</p>
+                        <button className="button_mua">Mua</button>
+                        <button className="button_detail">Chi tiết</button>
+                    </div>
+                ))}
+            </div>
         </div>
+
+
     );
 }
 
